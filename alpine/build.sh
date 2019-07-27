a60995ee0251739b0906d97a3367d526d53b2184
IInfinity <I.INF@Outlook.com>
2019-07-27 20:38:04 +0800

    perf(alpine): auto push manifest of alpine

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,2 +1,6 @@
 docker build --no-cache -t docker.io/iinfinity/alpine:$1 .
 docker push docker.io/iinfinity/alpine:$1
+
+rm -rf ~/.docker/manifests/*
+docker manifest create iinfinity/alpine iinfinity/alpine:$1
+docker manifest push --purge iinfinity/alpine:latest
