b88ded5225c3138b004e92feb18fc6b54370cec8
IInfinity <I.INF@Outlook.com>
2019-07-27 20:41:13 +0800

    perf(build): remove no-cache tag

--- alpine/build.sh
+++ alpine/build.sh
@@ -1,4 +1,4 @@
-docker build --no-cache -t docker.io/iinfinity/alpine:$1 .
+docker build -t docker.io/iinfinity/alpine:$1 .
 docker push docker.io/iinfinity/alpine:$1
 
 rm -rf ~/.docker/manifests/*
--- centos/build.sh
+++ centos/build.sh
@@ -1,2 +1,2 @@
-docker build --no-cache -t docker.io/iinfinity/centos:$1 .
+docker build -t docker.io/iinfinity/centos:$1 .
 docker push docker.io/iinfinity/centos:$1
