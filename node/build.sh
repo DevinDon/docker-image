7692bcbe08242f06ce2dd8c8f0217bae9535d254
Devin Don <DevinDon@Foxmail.com>
2020-01-12 17:58:37 +0800

    perf: add manifest command to each shell

--- node/build.sh
+++ node/build.sh
@@ -1,2 +1,6 @@
 docker build -t docker.io/iinfinity/node:$1 .
 docker push docker.io/iinfinity/node:$1
+
+rm -rf ~/.docker/manifests/*
+docker manifest create iinfinity/node iinfinity/node:$1
+docker manifest push --purge iinfinity/node:latest
