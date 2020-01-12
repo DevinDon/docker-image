949ecb02f4d4dfaaee6e8a5667dcb6a24c1ed55d
Devin Don <DevinDon@Foxmail.com>
2020-01-12 18:02:28 +0800

    fix: fix build index

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ then
 fi
 
 cd alpine && sh build.sh $arch && \
+cd ../nginx && sh build.sh $arch && \
 cd ../aria2 && sh build.sh $arch && \
-cd ../centos && sh build.sh $arch && \
+# cd ../centos && sh build.sh $arch && \
 cd ../gcc && sh build.sh $arch && \
 cd ../java && sh build.sh $arch && \
-cd ../nginx && sh build.sh $arch && \
 cd ../node && sh build.sh $arch && \
 cd ../python && sh build.sh $arch && \
 cd ../rust && sh build.sh $arch && \
