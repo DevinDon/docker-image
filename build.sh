5f468fc7cda8386c6b7500cb970902c79a70ec08
IInfinity <I.INF@outlook.com>
2020-03-21 00:56:48 +0800

    fix(build.sh): allow to build centos

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ fi
 cd alpine && sh build.sh $arch && \
 cd ../nginx && sh build.sh $arch && \
 cd ../aria2 && sh build.sh $arch && \
-# cd ../centos && sh build.sh $arch && \
+cd ../centos && sh build.sh $arch && \
 cd ../gcc && sh build.sh $arch && \
 cd ../java && sh build.sh $arch && \
 cd ../node && sh build.sh $arch && \
