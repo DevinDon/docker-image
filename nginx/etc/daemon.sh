5d79417fadf0b5e3e70d236644c677aa9910c0e0
IInfinity <I.INF@outlook.com>
2020-04-02 09:14:07 +0800

    perf(nginx): nginx run in frontground

--- nginx/etc/daemon.sh
+++ nginx/etc/daemon.sh
@@ -3,7 +3,7 @@ do
 
 if [ ! -f "/var/run/nginx.pid" ]
 then
-  echo $(date +%T%n%F)" Restart nginx Services " && nginx
+  echo $(date +%T%n%F)" Restart nginx Services " && nginx –g 'daemon off;'
 else
   sleep 120
 fi
