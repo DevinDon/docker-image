88363a97d7861a733135f94fa07d219e7a24e248
Devin Don <Devin@Don.red>
2020-04-10 01:26:00 +0800

    fix(storage): fix storage image builder

--- storage/build.sh
+++ storage/build.sh
@@ -1,4 +1,4 @@
-docker build --build-arg arch=$1 -t docker.io/iinfinity/clash:$1 .
+docker build --build-arg arch=$1 -t docker.io/iinfinity/storage:$1 .
 docker push docker.io/iinfinity/storage:$1
 
 rm -rf ~/.docker/manifests/*
