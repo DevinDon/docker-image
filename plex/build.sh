0af5a6037615b65e7f07cee5f0a971713ce8acf9
Devin Don <Devin@Don.red>
2020-04-10 17:03:47 +0800

    feat(plex): add plex image

--- plex/build.sh
+++ plex/build.sh
@@ -1,4 +1,20 @@
-docker build -t docker.io/iinfinity/plex:$1 .
+#!/bin/bash
+
+if [ $1 == "arm64" ]
+then
+  arch="aarch64"
+  platform="aarch64"
+else
+  arch="amd64"
+  platform="x86_64"
+fi
+
+echo $1 $arch $platform
+
+docker build \
+  --build-arg S6_OVERLAY_ARCH=$arch \
+  --build-arg PLEX_BUILD=linux-$platform \
+  -t docker.io/iinfinity/plex:$1 .
 docker push docker.io/iinfinity/plex:$1
 
 rm -rf ~/.docker/manifests/*
